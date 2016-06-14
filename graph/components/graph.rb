b6fb2cba14c7141636a9954fb3fe19eaeeb21516
Jeffrey Penkar <jspenkar@gmail.com>
2016-06-14 08:13:19 -0500

    Refactored structure of graphs.

--- graph/components/graph.rb
+++ graph/components/graph.rb
@@ -1,6 +1,23 @@
 class Graph
   attr_accessor :nodes
-  def init(nodes)
-    @nodes = nodes
+  def initialize
+    @nodes = Array.new
+  end
+
+  def link_exist?(start, fin, paths = 1)
+    if paths == 1
+      one_link(start, fin)
+    else
+      two_link(start, fin)
+    end
+  end
+
+  private
+  def one_link(start, fin)
+
+  end
+
+  def two_link(start, fin)
+
   end
 end
--- graph/components/node.rb
+++ graph/components/node.rb
@@ -1,8 +1,23 @@
 class Node
   attr_accessor :adjacent, :value, :status
-  def init(value, adjacent)
+  def initialize(value = nil)
     @value = value
-    @adjacent = adjacent
+    @adjacent = Array.new
     @status = 0
+  end
+
+  def reset
+    @status = 0
+  end
+
+  def add_adjacent(*adjacent)
+    @adjacent.push adjacent
+    @adjacent.uniq!
+  end
+
+  def remove_adjacent(*adjacent)
+    [*adjacent].each do |x|
+			@arr.slice!(@arr.index(x)) if @arr.index(x)
+		end
   end
 end
