ce8c4abf1b1cfc2cc07ecd80d576666412e781e3
Jeffrey Penkar <jspenkar@gmail.com>
2016-06-15 14:43:14 -0500

    LinkedList class created.

---
+++ linkedlist/components/linkedlist.rb
@@ -0,0 +1,8 @@
+class LinkedList
+  attr_accessor :head, :tail, :length
+  def initialize
+    @length = 0
+    @head = nil
+    @tail = nil
+  end
+end
--- linkedlist/spec/linkedlist_spec.rb
+++ linkedlist/spec/linkedlist_spec.rb
@@ -4,5 +4,8 @@ describe LinkedList do
   it "Is a LinkedList" do
     ll = LinkedList.new
     expect(ll.class.to_s).to eq('LinkedList')
+    expect(ll.head).to eq(nil)
+    expect(ll.tail).to eq(nil)
+    expect(ll.length).to eq(0)
   end
 end
