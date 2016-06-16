2272ceeb88f3f7a2d5fb9c012f26b3bcb8f87a5c
Jeffrey Penkar <jspenkar@gmail.com>
2016-06-16 08:09:12 -0500

    several methods / tests added.

--- linkedlist/components/linkedlist.rb
+++ linkedlist/components/linkedlist.rb
@@ -5,4 +5,46 @@ class LinkedList
     @head = nil
     @tail = nil
   end
+
+  def add(value=nil, name=nil)
+    node = LLNode.new(value, name)
+    if !@head
+      @head = node
+      @tail = node
+    else
+      cur = @tail
+      @tail.next = node
+      @tail = node
+      @tail.previous = cur
+    end
+    @length += 1
+  end
+
+  def add_head(value=nil, name=nil)
+    node = LLNode.new(value, name)
+    node.next = @head
+    iterate
+    @head.previous = node
+    @head = node
+  end
+
+  def remove_index(index)
+
+  end
+
+  def remove
+
+  end
+
+  def remove_tail
+    prev = @tail.previous
+    prev.next = nil
+    @tail = prev
+    iterate(false)
+  end
+
+  private
+  def iterate(un = true)
+    @length += un ? 1 : -1
+  end
 end
--- linkedlist/spec/linkedlist_spec.rb
+++ linkedlist/spec/linkedlist_spec.rb
@@ -8,4 +8,36 @@ describe LinkedList do
     expect(ll.tail).to eq(nil)
     expect(ll.length).to eq(0)
   end
+
+  it "Should be able to add a node" do
+    ll = LinkedList.new
+    ll.add(1,'name1')
+    expect(ll.length).to eq(1)
+    ll.add(2,'name2')
+    expect(ll.length).to eq(2)
+    expect(ll.head.value).to eq(1)
+    expect(ll.tail.value).to eq(2)
+  end
+
+  it "Should be able to add a node to the head if wanted." do
+    ll = LinkedList.new
+    ll.add(1,'name1')
+    expect(ll.length).to eq(1)
+    ll.add_head(2,'name2')
+    expect(ll.length).to eq(2)
+    expect(ll.head.value).to eq(2)
+    expect(ll.tail.value).to eq(1)
+  end
+
+  it "Should be able to remove the tail of the LinkedList" do
+    ll = LinkedList.new
+    ll.add(1,'name1')
+    ll.add(2,'name2')
+    ll.add(3,'name3')
+    expect(ll.length).to eq(3)
+    expect(ll.tail.value).to eq(3)
+    ll.remove_tail
+    expect(ll.length).to eq(2)
+    expect(ll.tail.value).to eq(2)
+  end
 end
