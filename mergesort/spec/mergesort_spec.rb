316eb2667d60efd61f950da993885476341c3e0a
Jeffrey Penkar <jspenkar@gmail.com>
2016-06-17 10:17:24 -0500

    Mergesort working and tests working.

--- mergesort/spec/mergesort_spec.rb
+++ mergesort/spec/mergesort_spec.rb
@@ -4,5 +4,16 @@ describe Array do
   it "should create an array normally." do
     expect(Array.new.class.to_s).to eq("Array")
   end
-  # puts Array.new.methods.sort
+
+  it "Should sort an array correctly" do
+    expect([5,4,3,2,1,6].mergesort).to eq([1,2,3,4,5,6])
+  end
+
+  it "Should sort an array correctly" do
+    expect(["z","y","x","w","v","u"].mergesort).to eq(["u", "v", "w", "x", "y", "z"])
+  end
+
+  it "Should sort an array correctly" do
+    expect((1..100).to_a.reverse.mergesort).to eq((1..100).to_a)
+  end
 end
