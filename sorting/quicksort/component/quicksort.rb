310b5f9cc5583e284fcd7862c4e66bb60f93b508
Jeffrey Penkar <jspenkar@gmail.com>
2016-06-18 20:00:16 -0500

    Non functioning quicksort method created.

--- sorting/quicksort/component/quicksort.rb
+++ sorting/quicksort/component/quicksort.rb
@@ -1,3 +1,14 @@
 class Array
-  
+  def quicksort
+    return self if self.length < 2
+    arr1 = Array.new
+    arr2 = Array.new
+    pivot = self[self.length / 2]
+
+  end
+
+  private
+  def partition(arr, left, right)
+    
+  end
 end
