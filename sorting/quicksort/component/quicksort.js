22b32a72dc17d908e720ad220a0671b898b43c58
jsp <jspenkar@gmail.com>
2016-09-06 23:39:46 -0500

    JavaScript quicksort added.

---
+++ sorting/quicksort/component/quicksort.js
@@ -0,0 +1,17 @@
+function quickSort(array){
+  let len = array.length;
+  if(len < 2) return array;
+  let lessr = [], great = [], equal = [];
+  let pivot = (array[0] + array[len-1]) / 2;
+  for(var i = 0; i < len; i++){
+    var num = array[i];
+    if(num < pivot){
+      lessr.push(num);
+    } else if (num > pivot){
+      great.push(num);
+    } else {
+      equal.push(num);
+    }
+  }
+  return [].concat(quickSort(lessr), equal, quickSort(great));
+}
