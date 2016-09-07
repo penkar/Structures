3c7c5f09173aa85294670606ca01d1b93a3cbd1e
jsp <jspenkar@gmail.com>
2016-09-06 23:21:00 -0500

    Quicksort.rb trimmed slightly.

--- sorting/quicksort/component/quicksort.rb
+++ sorting/quicksort/component/quicksort.rb
@@ -18,22 +18,4 @@ class Array
     less = less.quicksort
     return less + equal + gret
   end
-
-  # private
-  # def partition(arr, left, right)
-  #
-  # end
 end
-
-
-# function quicksort(array)
-#     less, equal, greater := three empty arrays
-#     if length(array) > 1
-#         pivot := select any element of array
-#         for each x in array
-#             if x < pivot then add x to less
-#             if x = pivot then add x to equal
-#             if x > pivot then add x to greater
-#         quicksort(less)
-#         quicksort(greater)
-#         array := concatenate(less, equal, greater)
