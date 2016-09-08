829ace776f827d5631caf22c8ff87ed4230f35f4
jsp <jspenkar@gmail.com>
2016-09-08 00:00:26 -0500

    BucketSort js added.

---
+++ sorting/bucketsort/component/bucketsort.js
@@ -0,0 +1,19 @@
+function bucketSort(array){
+  let tallest = array[0], shortest = array[0], nums = {}, array2 = [];
+  for(var i = 0; i < array.length; i++){
+    var num = array[i];
+    nums[num] = ((nums[num] || 0) + 1);
+    if(num > tallest){
+      tallest = num;
+    } else if(num < shortest){
+      shortest = num;
+    }
+  }
+  for(var j = shortest; j <= tallest; j++){
+    var renum = nums[j];
+    for(var k = 0; k < renum; k++){
+      array2.push(j);
+    }
+  }
+  return array2;
+}
