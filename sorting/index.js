04ca72b2bd4704246c472f7b6d520cd049eac8a0
Jeffrey Penkar <jspenkar@gmail.com>
2018-10-07 00:09:34 -0500

    Packages updated.

--- sorting/index.js
+++ sorting/index.js
@@ -2,6 +2,6 @@ import BubbleSort from './bubblesort/components/bubblesort.js';
 import QuickSort from './quicksort/component/quicksort.js';
 import BucketSort from './bucketsort/component/bucketsort.js';
 import MergeSort from './mergesort/component/mergesort.js';
-import SelectionSort from './mergesort/component/selectionsort.js';
+import SelectionSort from './selectionsort/component/selectionsort.js';
 
 export { BubbleSort, QuickSort, BucketSort, MergeSort, SelectionSort };
