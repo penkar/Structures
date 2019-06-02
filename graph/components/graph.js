ac84b0711423ac676be10dd7e0691c4d5b67611f
Jeffrey Penkar <jspenkar@gmail.com>
2019-06-01 22:30:08 -0500

    Graph searches refactored down in size.

--- graph/components/graph.js
+++ graph/components/graph.js
@@ -1,8 +1,8 @@
 export default class Graph {
-  constructor(verticieCount = 0) {
-    this.verticiesCount = verticieCount;
-    this.adjacentList = new Map();
-  }
+  adjacentList = new Map();
+  verticiesCount = 0;
+  getEdgesForVerticie = (vertice) => this.adjacentList.get(vertice);
+  printVerticies = () => [...this.adjacentList.keys()];
 
   addVertice(vertice) {
     this.verticiesCount++
@@ -10,83 +10,38 @@ export default class Graph {
   }
 
   addEdge(edge1, edge2, directed = false) {
-    if(edge1 === edge2) return null;
-
-    if(!this.adjacentList.has(edge1)) this.addVertice(edge1);
-    this.adjacentList.get(edge1).add(edge2);
-
-    if(!this.adjacentList.has(edge2)) this.addVertice(edge2);
-    if(!directed) this.adjacentList.get(edge2).add(edge1);
-  }
-
-  getEdgesForVerticie(vertice) {
-    return this.adjacentList.get(vertice);
-  }
-
-  printVerticies() {
-    return [...this.adjacentList.keys()];
-  }
-
-  breadthFirstSearch(start, end) {
-    let visited = new Object(), 
-      queue = [start], 
-      current = start, 
-      path = [], 
-      setup = new Map(),
-      connected = false;
-    for(let i of this.printVerticies()) visited[i] = false;
-
-    while(queue.length) {
-      if(current === end) {
-        connected = true;
-        break;
-      }
-
-      visited[current] = true;
-      let currentVerticies = this.getEdgesForVerticie(current);
-
-      for(let j of currentVerticies) {
-        setup.set(j, current);
-        if(visited[j] === false) queue.push(j); 
-      }
-      [current, ...queue] = queue;
-    }
-
-    if(connected) {
-      while(current && current != start) {
-        path.push(current);
-        current = setup.get(current);
-      }
-      path.push(start);
-      return path;
-    } else {
-      return false;
+    if(edge1 !== edge2) {
+      if(!this.adjacentList.has(edge1)) this.addVertice(edge1);
+      this.adjacentList.get(edge1).add(edge2);
+      if(!this.adjacentList.has(edge2)) this.addVertice(edge2);
+      if(!directed) this.adjacentList.get(edge2).add(edge1);
     }
   }
 
-  depthFirstSearch(start, end) {
-    let path = [], queue = [], current = start, setup = new Map(), connected = false;
+  breadthFirstSearch = (start, end) => this.search(start, end, true);
+  depthFirstSearch = (start, end) => this.search(start, end, false);
+  search(start, end, breadthFirst) {
+    let visited = new Object(), path = [], queue = [start], current = start, setup = new Map(), connected = false;
     while(current) {
       let currentVerticies = this.getEdgesForVerticie(current);
+      visited[current] = true;
       if(current === end) {
         connected = true;
         break;
       }
       for(let i of currentVerticies) setup.set(i, current);
-
-      queue = [...currentVerticies, ...queue];
+      if(breadthFirst) {
+        queue = [...queue, ...currentVerticies].filter((x) => (!visited[x]));
+      } else {
+        queue = [...currentVerticies, ...queue].filter((x) => (!visited[x]));
+      }
       [current, ...queue] = queue;
     }
-
-    if(connected) {
-      while(current && current != start) {
-        path.push(current);
-        current = setup.get(current);
-      }
-      path.push(start);
-      return path;
-    } else {
-      return false;
+    if(!connected) return false;
+    while(current && current != start) {
+      path.push(current);
+      current = setup.get(current);
     }
+    return [...path, start];
   }
 }
